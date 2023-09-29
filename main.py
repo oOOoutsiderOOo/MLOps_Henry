ff6804c9314f81f6469426aa1b2569a37500350f
Nicolás Ciancaglini <trancesoup@gmail.com>
2023-09-29 00:45:23 -0300

    lots of changes

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
 
+load_dotenv()
 app = FastAPI()
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/games")
+async def games():
+    return {"message": "Games"}
 
 def PlayTimeGenre( genero : str):
     pass
