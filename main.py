6f6e4139df910f2336257696754f59b095c18847
Nicolás Ciancaglini <trancesoup@gmail.com>
2023-09-29 00:56:09 -0300

    created virtual enviroment

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from fastapi import FastAPI
-import os
-from supabase import create_client, Client
-from dotenv import load_dotenv
 
-load_dotenv()
 app = FastAPI()
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
 
 
 @app.get("/")
