79bd332b7a490414cdc1010b014a95a9f97c1bfd
Nicolás Ciancaglini <trancesoup@gmail.com>
2023-09-29 06:14:36 -0300

    intentando cargar datos

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import pandas as pd
 app = FastAPI()
 
 print("Cargando datos...")
-df_items = pd.read_parquet('./src/cleaned/items.parquet')
-df_reviews = pd.read_parquet('./src/cleaned/reviews.parquet')
-df_users = pd.read_parquet('./src/cleaned/users.parquet')
-df_games = pd.read_parquet('./src/cleaned/games.parquet')
+try:
+    df_items = pd.read_parquet('./src/cleaned/items.parquet')
+    df_reviews = pd.read_parquet('./src/cleaned/reviews.parquet')
+    df_users = pd.read_parquet('./src/cleaned/users.parquet')
+    df_games = pd.read_parquet('./src/cleaned/games.parquet')
+except Exception as error:
+    print("Error al cargar los datos: ", error)
+    
 print("Datos cargados.")
 
 @app.get("/")
